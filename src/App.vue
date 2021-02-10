1d25aee284fdd31f4cca8091cabceb93f8e800ca
Your Name <you@example.com>
2021-02-10 21:48:59 +0900

    Vuejsの日々の学習の記録として残します

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="message1">
+    <h1>{{ message }}</h1>
+    <a :href="url">Firebase</a>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data:function(){
+    return{
+       message:"私のお気に入りのページです。",
+       url:"https://firebase.google.com/"
+      }
+    }
+  }
+
+</script>
+
+<style>
+.message1 {
+  text-align: center;
+  color: violet;
+  margin-top: 60px;
+}
+</style>
